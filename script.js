f2a534e2305df9e606e0d759e4a1cf3e3c542f7a
fame1987 <flamzaa911@gmail.com>
2024-12-16 09:32:53 +0700

    new test_v2

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log("Sivakon Wongsajam");
-console.log("Fame");
+function appendToResult(value) {
+    document.getElementById('result').value += value;
+}
+
+function clearResult() {
+    document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+    const resultField = document.getElementById('result');
+    try {
+        resultField.value = eval(resultField.value);
+    } catch (error) {
+        resultField.value = 'Error';
+    }
+}
